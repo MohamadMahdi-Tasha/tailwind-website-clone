45036d33a9e0fd10ad57edc6705115c7eed8c9f2
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-30 14:05:02 +0330

    added changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// Codes By Mahdi Tasha
+// Importing Part
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Variables
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+// Rendering Whole Page In Root Element
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <h1>Hello World</h1>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
