7415001d81610086c9bf4d013bd71d66abdcbf0d
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-04 16:22:49 +0330

    changed some structures in editorComponent

--- src/components/chunks/editorComponent.js
+++ src/components/chunks/editorComponent.js
@@ -5,18 +5,31 @@ import Prism from 'prismjs';
 import '../../assets/prismTheme.css';
 
 // Creating Functional Component And Exporting It As Defualt
-export default function EditorComponent({code, language}) {
+export default function EditorComponent({code, language, className}) {
     // Configuring Prism When Component Is Changed
     useEffect(() => Prism.highlightAll());
 
+    // Variables
+    const linesOfCode = code.split(/\r\n|\r|\n/).length;
+
     // Returning JSX
     return (
-        <div>
-            <pre>
-                <code className={`language-${language}`}>
-                    {code}
-                </code>
-            </pre>
+        <div className={className}>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div>{[...Array(linesOfCode)].map((e, i) => <h6 key={i}>{i}</h6>)}</div>
+                <div>
+                    <pre>
+                    <code className={`language-${language}`}>
+                        {code}
+                    </code>
+                </pre>
+                </div>
+            </div>
         </div>
     );
 }
