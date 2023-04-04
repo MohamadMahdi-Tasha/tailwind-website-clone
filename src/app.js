dbea3c0f8a39d0017a9756001e7f5dcc9e95f80f
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-04 18:46:36 +0330

    changed some codes in structures of first section

--- src/app.js
+++ src/app.js
@@ -33,34 +33,38 @@ const firstSectionsCode = `<figure class="absolute top-[50%] translate-y-[-50%] 
 export default function AppComponent() {
     // Returning JSX
     return (
-        <SectionComponent className={'border-b border-b-slate-100/5 bg-[url("./assets/img/img-sect-1.png")] bg-no-repeat bg-cover bg-bottom'}>
-            <HeaderComponent />
-            <ContainerComponent className='mb-[120px]'>
-                <h1 className={'font-extrabold text-white text-6xl text-center mb-5'}>Rapidly build modern websites <br/> without ever leaving your HTML.</h1>
-                <p className={'text-slate-400 text-center text-xl mb-9'}>
-                    A utility-first CSS framework packed with classes like
-                    <span className={'text-cyan-400'}> flex </span>,
-                    <span className={'text-cyan-400'}> pt-4 </span>,
-                    <span className={'text-cyan-400'}> text-center </span>
-                    and <br/>
-                    <span className={'text-cyan-400'}> rotate-90 </span>
-                    that can be composed to build any design, directly in your markup.
-                </p>
-                <div className={'flex justify-center'}>
-                    <a className='mr-7 w-32' href="#"><button tabIndex={'-1'} className={'rounded-lg border-t-2 border-t-white/30 bg-sky-500 hover:bg-sky-400 w-full py-3 text-white font-bold text-1xl'}>Get Started</button></a>
-                    <button className={'flex w-80 items-center justify-between bg-slate-800 hover:bg-slate-700 border-t-2 border-t-white/10 rounded-lg px-3'}>
-                        <h6 className='flex'>
-                            <IconComponent type={'search'} />
-                            <h6 className={'ml-2 text-slate-300'}>Quick Search ...</h6>
-                        </h6>
-                        <kbd className='text-slate-500'>Ctrl K</kbd>
-                    </button>
-                </div>
-            </ContainerComponent>
-            <ContainerComponent className='relative h-[500px]'>
-                <PreviewCodeComponent code={firstSectionsCode}/>
-                <EditorComponent language={'html'} code={firstSectionsCode} className={'w-[50%] absolute right-0'}/>
-            </ContainerComponent>
-        </SectionComponent>
+        <>
+            <SectionComponent className={'border-b border-b-slate-100/5 bg-[url("./assets/img/img-sect-1.png")] bg-no-repeat bg-cover bg-bottom'}>
+                <ContainerComponent>
+                    <HeaderComponent />
+                    <div className={'mb-[120px]'}>
+                        <h1 className={'font-extrabold text-white text-6xl text-center mb-5'}>Rapidly build modern websites <br/> without ever leaving your HTML.</h1>
+                        <p className={'text-slate-400 text-center text-xl mb-9'}>
+                            A utility-first CSS framework packed with classes like
+                            <span className={'text-cyan-400'}> flex </span>,
+                            <span className={'text-cyan-400'}> pt-4 </span>,
+                            <span className={'text-cyan-400'}> text-center </span>
+                            and <br/>
+                            <span className={'text-cyan-400'}> rotate-90 </span>
+                            that can be composed to build any design, directly in your markup.
+                        </p>
+                        <div className={'flex justify-center'}>
+                            <a className='mr-7 w-32' href="#"><button tabIndex={'-1'} className={'rounded-lg border-t-2 border-t-white/30 bg-sky-500 hover:bg-sky-400 w-full py-3 text-white font-bold text-1xl'}>Get Started</button></a>
+                            <button className={'flex w-80 items-center justify-between bg-slate-800 hover:bg-slate-700 border-t-2 border-t-white/10 rounded-lg px-3'}>
+                                <h6 className='flex'>
+                                    <IconComponent type={'search'} />
+                                    <h6 className={'ml-2 text-slate-300'}>Quick Search ...</h6>
+                                </h6>
+                                <kbd className='text-slate-500'>Ctrl K</kbd>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={'relative h-[500px]'}>
+                        <PreviewCodeComponent code={firstSectionsCode}/>
+                        <EditorComponent language={'html'} code={firstSectionsCode} className={'w-[50%] absolute right-0'}/>
+                    </div>
+                </ContainerComponent>
+            </SectionComponent>
+        </>
     );
 }
--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,7 +1,6 @@
 // Codes By Mahdi Tasha
 // Importing Part
 import IconComponent from "./chunks/iconComponent";
-import ContainerComponent from './chunks/containerComponent';
 import ListLinkComponent from './chunks/listLinkComponent';
 import ThemeChangerComponent from './chunks/themeChangerComponent';
 
@@ -9,22 +8,20 @@ import ThemeChangerComponent from './chunks/themeChangerComponent';
 export default function HeaderComponent() {
     // Returning JSX
     return (
-        <header className='mb-[99px]'>
-            <ContainerComponent className={'flex items-center justify-between gap-3 py-8'}>
-                <a href="#"><IconComponent type={'logo-typo'}/></a>
-                <ul className={'flex items-center gap-5'}>
-                    <ListLinkComponent>Docs</ListLinkComponent>
-                    <ListLinkComponent>Components</ListLinkComponent>
-                    <ListLinkComponent>Blog</ListLinkComponent>
-                    <ListLinkComponent>Showcase</ListLinkComponent>
-                    <li><div className={'w-[10px] h-[20px] border-r-[1px] border-r-white/40'} /></li>
-                    <li className={'relative'}><ThemeChangerComponent /></li>
-                    <ListLinkComponent
-                        link={"https://www.github.com/MohamadMahdi-Tasha"} >
-                        <IconComponent type={'github'}/>
-                    </ListLinkComponent>
-                </ul>
-            </ContainerComponent>
+        <header className='mb-[99px] flex items-center justify-between'>
+            <a href="#"><IconComponent type={'logo-typo'}/></a>
+            <ul className={'flex items-center gap-5'}>
+                <ListLinkComponent>Docs</ListLinkComponent>
+                <ListLinkComponent>Components</ListLinkComponent>
+                <ListLinkComponent>Blog</ListLinkComponent>
+                <ListLinkComponent>Showcase</ListLinkComponent>
+                <li><div className={'w-[10px] h-[20px] border-r-[1px] border-r-white/40'} /></li>
+                <li className={'relative'}><ThemeChangerComponent /></li>
+                <ListLinkComponent
+                    link={"https://www.github.com/MohamadMahdi-Tasha"} >
+                    <IconComponent type={'github'}/>
+                </ListLinkComponent>
+            </ul>
         </header>
     );
 }
