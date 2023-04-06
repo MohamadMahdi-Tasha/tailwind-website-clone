079cb9498b2fcc419e39d0a766c8ee7e8f4ff89c
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-06 17:01:12 +0330

    added some scripts for openening and closing moble header dropdown

--- src/components/chunks/mobileHeaderDropdown.js
+++ src/components/chunks/mobileHeaderDropdown.js
@@ -12,17 +12,27 @@ export default function MobileHeaderDropdown() {
     // Returning JSX
     return (
         <li className={'md:hidden block'}>
-            <button className={'flex items-center justify-center w-[32px] h-[32px]'} onClick={() => (isOpened) ? setOpened(false) : setOpened(true)}>
+            <button className={'flex items-center justify-center w-[32px] h-[32px]'} onClick={() => {
+                setOpened(true);
+                document.body.classList.add('overflow-hidden');
+            }}>
                 <IconComponent type={'vertical-dots'}/>
             </button>
             {
                 (isOpened) ? (
-                    <div className={'bg-slate-900/80 absolute top-0 left-0 w-full h-full backdrop-blur-sm p-4 flex justify-end items-start'}>
+                    <div className={'bg-slate-900/80 absolute z-40 top-0 left-0 w-full h-full backdrop-blur-sm p-4 flex justify-end items-start'}>
                         <div className={'w-80 bg-slate-800 border-t-2 border-t-slate-600/30 rounded-md flex flex-col gap-3'}>
                             <ul>
                                 <li className={'flex justify-between w-full'}>
                                     <a className={'w-full'} href="#"><button tabIndex={'-1'} className={'p-4 w-full text-left text-slate-400 font-bold text-sm hover:text-cyan-400'}>Docs</button></a>
-                                    <button className={'p-4 text-slate-400 hover:text-slate-300'} onClick={() => setOpened(false)}><IconComponent type={'x'}/></button>
+                                    <button
+                                        className={'p-4 text-slate-400 hover:text-slate-300'}
+                                        onClick={() => {
+                                            setOpened(false);
+                                            document.body.classList.remove('overflow-hidden');
+                                        }}>
+                                        <IconComponent type={'x'}/>
+                                    </button>
                                 </li>
                                 <li><a href="#"><button tabIndex={'-1'} className={'p-4 w-full text-slate-400 text-left font-bold text-sm hover:text-cyan-400'}>Components</button></a></li>
                                 <li><a href="#"><button tabIndex={'-1'} className={'p-4 w-full text-slate-400 text-left font-bold text-sm hover:text-cyan-400'}>Blog</button></a></li>
