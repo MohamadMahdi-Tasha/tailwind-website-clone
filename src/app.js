3d68f8f14095721abe326ad7962f48578135a7bf
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-04-09 17:08:27 +0330

    added some strcutures for rest of last sections we were working on

--- src/app.js
+++ src/app.js
@@ -208,6 +208,43 @@ export default function AppComponent() {
                         </div>
                     </ContainerComponent>
                 </div>
+                <ContainerComponent>
+                    <div>
+                        <h1>Not into component frameworks?</h1>
+                        <p>Use Tailwind's @apply directive to extract repeated utility patterns into custom CSS classes just by copying and pasting the list of class names.</p>
+                        <a href="#"><button>Learn More <IconComponent type={'chevron-right'}/></button></a>
+                        <div className="relative z-10 bg-white rounded-xl shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 dark:highlight-white/10">
+                            <article>
+                                <h2 className="text-lg font-semibold text-slate-900 pt-4 pb-2 px-4 sm:px-6 lg:px-4 xl:px-6 dark:text-slate-100 transition-opacity duration-[1.5s] delay-500 opacity-25">Weekly one-on-one</h2>
+                                <dl className="flex flex-wrap divide-y divide-slate-200 border-b border-slate-200 text-sm sm:text-base lg:text-sm xl:text-base dark:divide-slate-200/5 dark:border-slate-200/5">
+                                    <div className="px-4 sm:px-6 lg:px-4 xl:px-6 pb-4">
+                                        <dt className="sr-only">Date and time</dt>
+                                        <dd className="transition-opacity duration-[1.5s] delay-500 opacity-25">
+                                            <time dateTime="2020-11-15T10:00:00-05:00">Thu Nov 15, 2020 10:00am</time>
+                                            <time dateTime="2020-11-15T11:00:00-05:00">11:00am<span className="sr-only sm:not-sr-only"> EST</span></time>
+                                        </dd>
+                                    </div>
+                                    <div className="w-full flex-none flex items-center p-4 sm:p-6 lg:p-4 xl:p-6">
+                                        <dt className="w-2/5 sm:w-1/4 flex-none text-slate-900 font-medium dark:text-slate-300 transition-opacity duration-[1.5s] delay-500 opacity-25">Location</dt>
+                                        <dd className="transition-opacity duration-[1.5s] delay-500 opacity-25">Kitchener, <abbr title="Ontario">ON</abbr></dd>
+                                    </div>
+                                    <div className="w-full flex-none flex items-center p-4 sm:p-6 lg:p-4 xl:p-6">
+                                        <dt className="w-2/5 sm:w-1/4 flex-none text-slate-900 font-medium dark:text-slate-300 transition-opacity duration-[1.5s] delay-500 opacity-25">Description</dt>
+                                        <dd className="italic transition-opacity duration-[1.5s] delay-500 opacity-25">No meeting description</dd>
+                                    </div>
+                                    <div className="w-full flex-none flex items-center p-4 sm:py-5 sm:px-6 lg:p-4 xl:py-5 xl:px-6">
+                                        <dt className="w-2/5 sm:w-1/4 flex-none text-slate-900 font-medium dark:text-slate-300 transition-opacity duration-[1.5s] delay-500 opacity-25">Attendees</dt>
+                                        <dd className="text-sm font-medium text-slate-700 bg-slate-100 rounded-full py-1 px-3 dark:bg-slate-700 dark:text-slate-300 transition-opacity duration-[1.5s] delay-500 opacity-25">Andrew McDonald</dd>
+                                    </div>
+                                </dl>
+                                <div className="grid grid-cols-2 gap-x-4 sm:gap-x-6 lg:gap-x-4 xl:gap-x-6 p-4 sm:px-6 sm:py-5 lg:p-4 xl:px-6 xl:py-5">
+                                    <div className="text-base font-medium rounded-lg bg-slate-100 text-slate-900 py-3 text-center cursor-pointer dark:bg-slate-600 dark:text-slate-400 dark:highlight-white/10">Decline</div>
+                                    <div className="text-base font-medium rounded-lg bg-sky-500 text-white py-3 text-center cursor-pointer dark:highlight-white/20">Accept</div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </ContainerComponent>
             </SectionComponent>
         </>
     );
